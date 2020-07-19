7a612908c7f1985612807b3a4608981efbb72967
Gary Linscott <glinscott@gmail.com>
2020-07-19 15:52:07 -0700

    Mobility bitboard experiment

---
+++ src/eval/nnue/features/mobility.cpp
@@ -0,0 +1,74 @@
+﻿#if defined(EVAL_NNUE)
+
+#include "mobility.h"
+#include "index_list.h"
+
+namespace Eval {
+
+namespace NNUE {
+
+namespace Features {
+
+void addIndex(Color Us, Bitboard b, int base, IndexList* active) {
+  while (b) {
+    Square s = pop_lsb(&b);
+    if (Us == BLACK)
+      s = Inv(s);
+    active->push_back(base + s);
+  }
+}
+
+// Get a list of indices with a value of 1 among the features
+void Mobility::AppendActiveIndices(
+    const Position& pos, Color perspective, IndexList* active) {
+  // do nothing if array size is small to avoid compiler warning
+  if (RawFeatures::kMaxActiveDimensions < kMaxActiveDimensions) return;
+
+  int base = 0;
+  for (PieceType pt = PAWN; pt <= KING; ++pt) {
+    Bitboard b = pos.state()->mobility[perspective][pt - 1];
+    addIndex(perspective, b, base, active);
+    base += 64;
+  }
+  Color them = ~perspective;
+  for (PieceType pt = PAWN; pt <= KING; ++pt) {
+    Bitboard b = pos.state()->mobility[them][pt - 1];
+    addIndex(them, b, base, active);
+    base += 64;
+  }
+}
+
+// Get a list of indices whose values have changed from the previous one in the feature quantity
+void Mobility::AppendChangedIndices(
+    const Position& pos, Color perspective,
+    IndexList* removed, IndexList* added) {
+  const StateInfo* st = pos.state()->previous;
+
+  int add = added->size(), rem = removed->size();
+
+  int base = 0;
+  for (PieceType pt = PAWN; pt <= KING; ++pt) {
+    Bitboard b = pos.state()->mobility[perspective][pt - 1] ^ st->mobility[perspective][pt - 1];
+    addIndex(perspective, b & pos.state()->mobility[perspective][pt - 1], base, added);
+    addIndex(perspective, b & st->mobility[perspective][pt - 1], base, removed);
+    base += 64;
+  }
+  Color them = ~perspective;
+  for (PieceType pt = PAWN; pt <= KING; ++pt) {
+    Bitboard b = pos.state()->mobility[them][pt - 1] ^ st->mobility[them][pt - 1];
+    addIndex(them, b & pos.state()->mobility[them][pt - 1], base, added);
+    addIndex(them, b & st->mobility[them][pt - 1], base, removed);
+    base += 64;
+  }
+  add = added->size() - add;
+  rem = removed->size() - rem;
+  // printf("add %d rem %d tot %d\n", add, rem, add + rem);
+}
+
+}  // namespace Features
+
+}  // namespace NNUE
+
+}  // namespace Eval
+
+#endif  // defined(EVAL_NNUE)
---
+++ src/eval/nnue/features/mobility.h
@@ -0,0 +1,51 @@
+﻿//Definition of input feature P of NNUE evaluation function
+
+#ifndef _NNUE_FEATURES_MOBILITY_H_
+#define _NNUE_FEATURES_MOBILITY_H_
+
+#if defined(EVAL_NNUE)
+
+#include "../../../evaluate.h"
+#include "features_common.h"
+
+namespace Eval {
+
+namespace NNUE {
+
+namespace Features {
+
+// Mobility
+//
+// Encoded as a bitboard per piece type.  Not incremental.  Mobility
+// is "raw" mobility - we rely on the net to interpret "safe".
+class Mobility {
+ public:
+  // feature quantity name
+  static constexpr const char* kName = "Mobility";
+  // Hash value embedded in the evaluation function file
+  static constexpr std::uint32_t kHashValue = 0x62924F91u;
+  // number of feature dimensions (bitboard per piece type)
+  static constexpr IndexType kDimensions = 64 * 6 * 2;
+  // The maximum value of the number of indexes whose value is 1 at the same time among the feature values
+  static constexpr IndexType kMaxActiveDimensions = 32 * 6 * 2;
+  // Timing of full calculation instead of difference calculation
+  static constexpr TriggerEvent kRefreshTrigger = TriggerEvent::kNone;
+
+  // Get a list of indices with a value of 1 among the features
+  static void AppendActiveIndices(const Position& pos, Color perspective,
+                                  IndexList* active);
+
+  // Get a list of indices whose values have changed from the previous one in the feature quantity
+  static void AppendChangedIndices(const Position& pos, Color perspective,
+                                   IndexList* removed, IndexList* added);
+};
+
+}  // namespace Features
+
+}  // namespace NNUE
+
+}  // namespace Eval
+
+#endif  // defined(EVAL_NNUE)
+
+#endif
